45dbe125f63930afff6e301a8b35553f3d2340aa
openbible <59015538+OpenBible3438@users.noreply.github.com>
2023-06-20 16:40:47 +0900

    [20230620] JSONData
    - Image Asset 추가
    - Model 객체 생성
    - JSON Data File 불러오기

--- Project5-TableWithDynamicCell/Project5-TableWithDynamicCell/Sources/Controllers/ArtistListViewController.swift
+++ Project5-TableWithDynamicCell/Project5-TableWithDynamicCell/Sources/Controllers/ArtistListViewController.swift
@@ -8,17 +8,39 @@
 import UIKit
 
 class ArtistListViewController: UIViewController {
+    
+    var model: Model!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
+        loadArtistData()
     }
     
     func setUI() {
         self.title = "Artistry"
     }
-
+    
+    func loadArtistData() {
+        // artists.json 파일 경로
+        guard let path = Bundle.main.path(forResource: "artists", ofType: "json") else {
+            return
+        }
+        
+        // 경로에 있는 파일을 String으로 가져오기
+        guard let jsonString = try? String(contentsOfFile: path) else {
+            return
+        }
+        
+        // String -> Model 객체
+        let data = jsonString.data(using: .utf8)!
+        guard let parsedData = try? JSONDecoder().decode(Model.self, from: data) else {
+            return
+        }
+        
+        model = parsedData
+    }
 
 }
 
